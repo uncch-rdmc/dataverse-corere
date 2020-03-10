6455b0daea6b62fe38b70c804a974475eaad63f5
matthew-a-dunlap <matthew.a.dunlap@gmail.com>
2020-03-10 17:38:54 -0400

    Consolidate templates

--- corere/main/views/main.py
+++ corere/main/views/main.py
@@ -73,33 +73,33 @@ def edit_manuscript(request, id=None):
     return render(request, 'main/form_create_manuscript.html', {'form': form, 'id': id})
 
 class GenericCorereObjectView(View):
-    transition_form_button_title = None
+    transition_button_title = None
     form = None
-    obj_class = None #should probably rename to make clear its not an instantiation
-    template = None
+    model = None
+    template = 'main/form_object_generic.html'
     redirect = '/'
     parent_reference_name = None
     parent_id_name = None
-    parent_obj_class = None #should probably rename to make clear its not an instantiation
+    parent_model = None
     object_friendly_name = None
     read_only = False
     http_method_names = ['get', 'post']
 
     def dispatch(self, request, *args, **kwargs): 
         if kwargs.get('id'):
-            self.obj = get_object_or_404(self.obj_class, id=kwargs.get('id'))
+            self.obj = get_object_or_404(self.model, id=kwargs.get('id'))
             self.message = 'Your '+self.object_friendly_name +' has been updated!'
         elif not self.read_only: #kwargs.get(self.parent_id_name) and 
-            self.obj = self.obj_class()
-            if(self.parent_obj_class is not None):
-                setattr(self.obj, self.parent_reference_name, get_object_or_404(self.parent_obj_class, id=kwargs.get(self.parent_id_name)))
+            self.obj = self.model()
+            if(self.parent_model is not None):
+                setattr(self.obj, self.parent_reference_name, get_object_or_404(self.parent_model, id=kwargs.get(self.parent_id_name)))
             self.message = 'Your new '+self.object_friendly_name +' has been created!'
         else:
             print("ERROR")
         self.form = self.form(self.request.POST or None, self.request.FILES or None, instance=self.obj)
         self.notes = []
         try:
-            self.obj_class._meta.get_field('notes')
+            self.model._meta.get_field('notes')
             for note in self.obj.notes.all():
                 if request.user.has_perm('view_note', note):
                     self.notes.append(note)
@@ -110,18 +110,18 @@ class GenericCorereObjectView(View):
         return super(GenericCorereObjectView, self).dispatch(request,*args, **kwargs)
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template, {'form': self.form, 'notes': self.notes })
+        return render(request, self.template, {'form': self.form, 'notes': self.notes, 'transition_text': self.transition_button_title, 'read_only': self.read_only })
 
     def post(self, request, *args, **kwargs):
         if self.form.is_valid():
             self.form.save()
-            if(self.transition_form_button_title and request.POST['submit'] == self.transition_form_button_title): #This checks to see which form button was used. There is probably a more precise way to check
+            if(self.transition_button_title and request.POST['submit'] == self.transition_button_title): #This checks to see which form button was used. There is probably a more precise way to check
                 self.transition_if_allowed(request, *args, **kwargs)
             messages.add_message(request, messages.INFO, self.message)
             return redirect(self.redirect)
         else:
             print(form.errors) #Handle exception better
-        return render(request, self.template, {'form': self.form, 'notes': self.notes })
+        return render(request, self.template, {'form': self.form, 'notes': self.notes, 'transition_text': self.transition_button_title, 'read_only': self.read_only })
 
 
     ######## Custom class functions. You will want to override some of these. #########
@@ -135,15 +135,13 @@ class ReadOnlyCorereMixin(object):
     http_method_names = ['get']
 
 
-
 ################################################################################################
 
 class ManuscriptEditView(GenericCorereObjectView):
-    transition_form_button_title = 'Save and Assign to Authors'
+    transition_button_title = 'Save and Assign to Authors'
     form = ManuscriptForm
-    template = 'main/form_create_manuscript.html'
     object_friendly_name = 'manuscript'
-    obj_class = Manuscript
+    model = Manuscript
 
     def transition_if_allowed(self, request, *args, **kwargs):
         if not fsm_check_transition_perm(self.obj.begin, request.user): 
@@ -158,17 +156,17 @@ class ManuscriptEditView(GenericCorereObjectView):
 
 class ManuscriptReadView(ReadOnlyCorereMixin, ManuscriptEditView):
     form = ReadOnlyManuscriptForm
-    template = 'main/form_view_manuscript.html'
+
 
 class SubmissionEditView(GenericCorereObjectView):
-    transition_form_button_title = 'Submit for Review'
+    transition_button_title = 'Submit for Review'
     form = SubmissionForm
-    template = 'main/form_create_submission.html'
+    
     parent_reference_name = 'manuscript'
     parent_id_name = "manuscript_id"
-    parent_obj_class = Manuscript
+    parent_model = Manuscript
     object_friendly_name = 'submission'
-    obj_class = Submission
+    model = Submission
 
     def transition_if_allowed(self, request, *args, **kwargs):
         if not fsm_check_transition_perm(self.obj.submit, request.user): 
@@ -183,18 +181,16 @@ class SubmissionEditView(GenericCorereObjectView):
 
 class SubmissionReadView(ReadOnlyCorereMixin, SubmissionEditView):
     form = ReadOnlySubmissionForm
-    template = 'main/form_view_submission.html'
 
 
 class CurationEditView(GenericCorereObjectView):
-    transition_form_button_title = 'Submit and Progress'
+    transition_button_title = 'Submit and Progress'
     form = CurationForm
-    template = 'main/form_create_curation.html'
     parent_reference_name = 'submission'
     parent_id_name = "submission_id"
-    parent_obj_class = Submission
+    parent_model = Submission
     object_friendly_name = 'curation'
-    obj_class = Curation
+    model = Curation
     redirect = '/'
 
     def transition_if_allowed(self, request, *args, **kwargs):
@@ -210,18 +206,16 @@ class CurationEditView(GenericCorereObjectView):
 
 class CurationReadView(ReadOnlyCorereMixin, CurationEditView):
     form = ReadOnlyCurationForm
-    template = 'main/form_view_curation.html'
 
 
 class VerificationEditView(GenericCorereObjectView):
-    transition_form_button_title = 'Submit and Progress'
+    transition_button_title = 'Submit and Progress'
     form = VerificationForm
-    template = 'main/form_create_verification.html'
     parent_reference_name = 'submission'
     parent_id_name = "submission_id"
-    parent_obj_class = Submission
+    parent_model = Submission
     object_friendly_name = 'verification'
-    obj_class = Verification
+    model = Verification
     redirect = '/'
 
     def transition_if_allowed(self, request, *args, **kwargs):
@@ -237,141 +231,9 @@ class VerificationEditView(GenericCorereObjectView):
 
 class VerificationReadView(ReadOnlyCorereMixin, VerificationEditView):
     form = ReadOnlyVerificationForm
-    template = 'main/form_view_verification.html'
 
 ################################################################################################
 
-def view_submission(request, manuscript_id=None, id=None):
-    submission = get_object_or_404(Submission, id=id)
-    form = ReadOnlySubmissionForm(instance=submission)
-    notes = []
-    for note in submission.submission_notes.all():
-        if request.user.has_perm('view_note', note):
-            notes.append(note)
-        else:
-            print("user did not have permission for note: " + note.text)
-    return render(request, 'main/form_view_submission.html', {'form': form, 'id': id, 'notes': notes })
-
-def edit_submission(request, manuscript_id=None, id=None):
-    if id:
-        submission = get_object_or_404(Submission, id=id)
-        message = 'Your submission has been updated!'
-    else:
-        submission = Submission()
-        submission.manuscript = get_object_or_404(Manuscript, id=manuscript_id)
-        message = 'Your new submission has been created!'
-    form = SubmissionForm(request.POST or None, request.FILES or None, instance=submission)
-    notes = []
-    for note in submission.submission_notes.all():
-        if request.user.has_perm('view_note', note):
-            notes.append(note)
-        else:
-            print("user did not have permission for note: " + note.text)
-    if request.method == 'POST':
-        if form.is_valid():
-            form.save()
-            if(request.POST['submit'] == 'Submit for Review'): #This checks to see which form button was used. There is probably a more precise way to check
-                if not fsm_check_transition_perm(submission.submit, request.user): 
-                    print("PermissionDenied")
-                    raise PermissionDenied
-                try: #TODO: only do this if the reviewer selects a certain form button
-                    submission.submit()
-                    submission.save()
-                except TransactionNotAllowed:
-                    print("TransitionNotAllowed") #Handle exception better
-                    raise
-
-            messages.add_message(request, messages.INFO, message)
-            return redirect('/')
-        else:
-            print(form.errors) #Handle exception better
-    return render(request, 'main/form_create_submission.html', {'form': form, 'id': id, 'notes': notes })
-
-
-
-def view_curation(request, submission_id=None, id=None):
-    curation = get_object_or_404(Curation, id=id)
-    form = ReadOnlyCurationForm(instance=curation)
-    notes = []
-    for note in curation.curation_notes.all():
-        if request.user.has_perm('view_note', note):
-            notes.append(note)
-        else:
-            print("user did not have permission for note: " + note.text)
-    return render(request, 'main/form_view_curation.html', {'form': form, 'id': id, 'notes': notes })
-
-def edit_curation(request, submission_id=None, id=None):
-    if id:
-        curation = get_object_or_404(Curation, id=id)
-        message = 'Your curation has been updated!'
-    else:
-        curation = Curation()
-        curation.submission = get_object_or_404(Submission, id=submission_id)
-        message = 'Your new curation has been created!'
-    form = CurationForm(request.POST or None, request.FILES or None, instance=curation)
-    notes = []
-    for note in curation.curation_notes.all():
-        if request.user.has_perm('view_note', note):
-            notes.append(note)
-        else:
-            print("user did not have permission for note: " + note.text)
-    if request.method == 'POST':
-        if form.is_valid():
-            form.save()
-            if(request.POST['submit'] == 'Submit and Progress'): #This checks to see which form button was used. There is probably a more precise way to check
-                try:
-                    curation.submission.review()
-                    curation.submission.save()
-                except TransactionNotAllowed:
-                    pass #We do not do review if the statuses don't align
-
-            messages.add_message(request, messages.INFO, message)
-            return redirect('/')
-        else:
-            print(form.errors) #Handle exception better
-    return render(request, 'main/form_create_curation.html', {'form': form, 'id': id, 'notes': notes})
-
-def view_verification(request, submission_id=None, id=None):
-    verification = get_object_or_404(Verification, id=id)
-    form = ReadOnlyVerificationForm(instance=verification)
-    notes = []
-    for note in verification.verification_notes.all():
-        if request.user.has_perm('view_note', note):
-            notes.append(note)
-        else:
-            print("user did not have permission for note: " + note.text)
-    return render(request, 'main/form_view_verification.html', {'form': form, 'id': id, 'notes': notes })
-
-def edit_verification(request, submission_id=None, id=None):
-    if id:
-        verification = get_object_or_404(Verification, id=id)
-        message = 'Your verification has been updated!'
-    else:
-        verification = Verification()
-        verification.submission = get_object_or_404(Submission, id=submission_id)
-        message = 'Your new verification has been created!'
-    form = VerificationForm(request.POST or None, request.FILES or None, instance=verification)
-    notes = []
-    for note in verification.verification_notes.all():
-        if request.user.has_perm('view_note', note):
-            notes.append(note)
-        else:
-            print("user did not have permission for note: " + note.text)
-    if request.method == 'POST':
-        if form.is_valid():
-            form.save()
-            if(request.POST['submit'] == 'Submit and Progress'): #This checks to see which form button was used. There is probably a more precise way to check
-                try: 
-                    verification.submission.review()
-                    verification.submission.save()
-                except TransactionNotAllowed:
-                    pass #We do not do review if the statuses don't align
-
-            messages.add_message(request, messages.INFO, message)
-            return redirect('/')
-        else:
-            print(form.errors) #Handle exception better
-    return render(request, 'main/form_create_verification.html', {'form': form, 'id': id, 'notes': notes})
 
 def edit_note(request, id=None, submission_id=None, curation_id=None, verification_id=None):
     if id:
