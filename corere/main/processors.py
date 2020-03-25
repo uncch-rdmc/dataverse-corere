709bf85cf59726a4d1edc0485425e7df69aa689a
matthew-a-dunlap <matthew.a.dunlap@gmail.com>
2020-03-25 00:50:50 -0400

    Upload directly to gitlab from dropzone
    Deals with base64 encoding, header stripping, etc.
    
    Needs to be functionally flushed out, but works.

--- corere/main/processors.py
+++ corere/main/processors.py
@@ -4,4 +4,8 @@ import os
 def export_vars(request):
     data = {}
     data['BINDER_ADDR_ENV'] = os.environ["BINDER_ADDR"]
+    data['GIT_CONFIG_URL_ENV'] = os.environ["GIT_CONFIG_URL"]
+    data['GIT_LAB_URL_ENV'] = os.environ["GIT_LAB_URL"]
+    data['GIT_API_VERSION_ENV'] = os.environ["GIT_API_VERSION"]
+    data['GIT_PRIVATE_TOKEN_ENV_MAD_DELETE'] = os.environ["GIT_PRIVATE_TOKEN"]
     return data
