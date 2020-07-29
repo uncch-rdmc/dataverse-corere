3a5bcbc51d72356c0f64b76129e700664ce70682
matthew-a-dunlap <matthew.a.dunlap@gmail.com>
2020-07-29 14:20:14 -0400

    Use docker secrets

---
+++ docker/secret.web.sample.sh
@@ -0,0 +1,19 @@
+export DJANGO_SECRET_KEY=
+export GIT_PRIVATE_ADMIN_TOKEN=
+
+### SOCIAL AUTH ###
+# Values used for Google OAuth2. See https://python-social-auth.readthedocs.io/en/latest/backends/google.html
+export SOCIAL_AUTH_GOOGLE_OAUTH2_KEY=
+export SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET=
+
+# Values used for Github OAuth2.
+export SOCIAL_AUTH_GITHUB_OAUTH2_KEY=
+export SOCIAL_AUTH_GITHUB_OAUTH2_SECRET=
+
+export DJANGO_SUPERUSER_USERNAME=
+export DJANGO_SUPERUSER_EMAIL=
+export DJANGO_SUPERUSER_PASSWORD=
+
+#NOTE: must be changed in db files as well
+export POSTGRES_USER=
+export POSTGRES_PASSWORD=
---
+++ env.secret.sample.sh
@@ -0,0 +1,14 @@
+export DJANGO_SECRET_KEY=
+export GIT_PRIVATE_ADMIN_TOKEN=
+
+### SOCIAL AUTH ###
+# Values used for Google OAuth2. See https://python-social-auth.readthedocs.io/en/latest/backends/google.html
+export SOCIAL_AUTH_GOOGLE_OAUTH2_KEY=
+export SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET=
+
+# Values used for Github OAuth2.
+export SOCIAL_AUTH_GITHUB_OAUTH2_KEY=
+export SOCIAL_AUTH_GITHUB_OAUTH2_SECRET=
+
+export POSTGRES_USER=
+export POSTGRES_PASSWORD=
