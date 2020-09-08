8bf28139ceccc4fefa864f951707aa1383ba18d3
matthew-a-dunlap <matthew.a.dunlap@gmail.com>
2020-09-08 16:02:57 -0400

    Fix dropdown template erroring during tests

--- corere/main/templatetags/auth_extras.py
+++ corere/main/templatetags/auth_extras.py
@@ -9,7 +9,7 @@ register = template.Library()
 @register.filter(name='has_group')
 def has_group(user, group_name): 
     group = Group.objects.get(name=group_name) 
-    return True if group in user.groups.all() else False
+    return True if ((user is not None) and (group in user.groups.all())) else False
 
 @register.filter(name='has_global_perm')
 def has_global_perm(user, perm_name):
