fd81c4361667703d49ff4e74ba00985c3aebc62b
matthew-a-dunlap <matthew.a.dunlap@gmail.com>
2020-10-20 17:32:58 -0400

    Require python-gitlab fork
    Also disable email in production (for now)

--- corere/settings/production.py
+++ corere/settings/production.py
@@ -13,13 +13,15 @@ ALLOWED_HOSTS = ['*'] #Should find a better kube solution
 
 DISABLE_GIT = False
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 #These settings are based upon gmail, switch to your prefered smtp service
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
-EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
-EMAIL_PORT = 587
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
+# EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
+# EMAIL_PORT = 587
 
 COMPRESS_OFFLINE = True
 COMPRESS_ENABLED = False
