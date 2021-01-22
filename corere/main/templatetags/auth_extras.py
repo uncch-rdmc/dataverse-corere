999b6f4bacfb99119d75ed8e3ec9115dcec156e1
matthew-a-dunlap <matthew.a.dunlap@gmail.com>
2021-01-22 16:48:14 -0500

    Move manuscript buttons to manuscript section
    This meant redoing the buttons directly, not in a datatable

--- corere/main/templatetags/auth_extras.py
+++ corere/main/templatetags/auth_extras.py
@@ -1,5 +1,7 @@
 from django import template
 from django.contrib.auth.models import Group, Permission
+from django_fsm import has_transition_perm
+from corere.main.models import Manuscript
 
 register = template.Library()
 
@@ -14,3 +16,11 @@ def has_group(user, group_name):
 @register.filter(name='has_global_perm')
 def has_global_perm(user, perm_name):
     return user.has_perm(perm_name)
+
+#TODO: make generalized if we need this function for other objects (e.g. submission)
+@register.simple_tag
+def manuscript_has_transition_perm(user, obj_id, perm_func_name):
+    manuscript = Manuscript.objects.get(id=int(obj_id))
+    perm_func = getattr(manuscript, perm_func_name)
+
+    return has_transition_perm(perm_func, user)
