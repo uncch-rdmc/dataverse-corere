d6ab92dafbde407ac56a639554b41fa84a5f09ed
matthew-a-dunlap <matthew.a.dunlap@gmail.com>
2022-02-08 18:59:46 -0500

    Add file buttons (partially functional), styling, etc

--- corere/apps/file_datatable/views.py
+++ corere/apps/file_datatable/views.py
@@ -5,32 +5,42 @@ from django_datatables_view.base_datatable_view import BaseDatatableView
 class FileBaseDatatableView(BaseDatatableView):
     max_display_length = 10000
 
-    def get_columns(self):
-        columns = [['download_button',''],['delete_button',''],['path','File Path'],['name','File Name'],['change_detection']]
-        return columns
+    #page_length = 10000
 
-    def prepare_results(self, qs):
-        data = []
+    columns = ['path','name',]
 
-        data.append(self.get_columns()) #adds headers to grab in js for dynamic support
+    order_columns = ['path','name']
 
-        print(self._columns)
-        print(qs)
-        for item in qs:
-            print(item)
-            if self.is_data_list:
-                data.append([self.render_column(item, column) for column in self._columns])
-            else:
-                row = {col_data['data']: self.render_column(item, col_data['data']) for col_data in self.columns_data}
-                data.append(row)
+    # def prepare_results(self, qs):
+    #     data = []
+
+    #     data.append(self.get_columns()) #adds headers to grab in js for dynamic support
+
+    #     print(self._columns)
+    #     print(qs)
+    #     for item in qs:
+    #         print(item)
+    #         if self.is_data_list:
+    #             data.append([self.render_column(item, column) for column in self._columns])
+    #         else:
+    #             row = {col_data['data']: self.render_column(item, col_data['data']) for col_data in self.columns_data}
+    #             data.append(row)
         
-        print("HEY")
-        return data
+    #     print("HEY")
+    #     return data
 
-    def render_column(self, file, column):
+    def render_column(self, row, column):
         #these string matches aren't the most exact, but fine for now
         # if column[0] == 'roles':
         #     return ', '.join(map(str, user.groups.filter(name__contains='Role')))
         # if column[0] == 'assigned_manuscripts':
         #     return user.groups.filter(name__contains='Manuscript').exclude(name__endswith=c.GROUP_COMPLETED_SUFFIX).count()
-        return super(FileBaseDatatableView, self).render_column(file, column[0])
+        return super(FileBaseDatatableView, self).render_column(row, column)
+
+    #Defining this stop django-datatable-view from only returning 10 results. There may be a better way. 
+    # https://datatables.net/forums/discussion/36649/only-10-records-is-processed has a bit more info but not much
+    def paging(self, qs):
+        """ Paging
+        """
+
+        return qs
