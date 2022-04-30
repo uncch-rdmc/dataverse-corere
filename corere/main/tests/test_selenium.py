58ac484e56a2f0bc16ebe9cd27383d42bfcc9625
matthew-a-dunlap <matthew.a.dunlap@gmail.com>
2022-04-30 17:20:19 -0400

    Incomplete test of multiple users and accesses
    wrote helper method to call many urls succinctly, using selenium wire to get response codes
    remove hardcoded test strings, we get object ids instead

--- corere/main/tests/test_selenium.py
+++ corere/main/tests/test_selenium.py
@@ -1,5 +1,5 @@
 from django.test import LiveServerTestCase
-from selenium import webdriver
+from seleniumwire import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
@@ -7,16 +7,17 @@ import time, unittest
 from selenium.webdriver.support.ui import WebDriverWait, Select
 
 from corere.main import models as m
+from corere.main import constants as c
 from corere.apps.wholetale import models as wtm
 
 
 # Part of this testing is based upon finding elements. If they aren't available, the test errors out.
 # TODO: Future tests streamline login: https://stackoverflow.com/questions/22494583/login-with-code-when-using-liveservertestcase-with-django
-#@unittest.skip("Don't want to test, broke after applying bootstrap, will come back to it when we do more exhaustive testing")
 class LoggingInTestCase(LiveServerTestCase):
     def setUp(self):
         self.selenium = webdriver.Chrome()
-        m.User.objects.create_superuser('admin', 'admin@admin.com', 'password')
+        m.User.objects.create_superuser('admin', 'admin@test.test', 'password')
+
         #We need add the other option for compute env with the current implementation
         wtm.ImageChoice.objects.get_or_create(wt_id="Other", name="Other", show_last=True)
 
@@ -26,11 +27,14 @@ class LoggingInTestCase(LiveServerTestCase):
         self.selenium.quit()
         super(LoggingInTestCase, self).tearDown()
 
-    #@unittest.skip("Don't want to test")
-    def test_admin_full_workflow_no_edition_no_dataverse(self):
+    # This tests most of the flow with all actions done by an admin.
+    # Not tested: Edition, Dataverse, Files, Whole Tale
+    # TODO: Hardcode no edition setting
+    @unittest.skip("testing others now")
+    def test_admin_only_mostly_full_workflow(self):
         selenium = self.selenium
 
-        ##### ADMIN LOG IN #####
+        ##### ADMIN LOGIN #####
 
         selenium.get(self.live_server_url + "/admin")
         self.assertIn("Log in", selenium.title)
@@ -88,13 +92,15 @@ class LoggingInTestCase(LiveServerTestCase):
 
         ##### ASSIGN AUTHOR TO MANUSCRIPT #####
 
-        selenium.get(self.live_server_url+"/manuscript/1/addauthor/")
+        manuscript = m.Manuscript.objects.latest('updated_at')
+        selenium.get(self.live_server_url + "/manuscript/" + str(manuscript.id) + "/addauthor/")
         #We don't fill out any fields for this form beacuse it is auto-populated with manuscript info
         manuscript_add_author_submit = selenium.find_element_by_xpath('//*[@id="content"]/form/input[4]')
         manuscript_add_author_submit.send_keys(Keys.RETURN)
         selenium.switch_to.alert.accept()
 
         ##### MANUSCRIPT LANDING #####
+
         manuscript_create_submit_continue = selenium.find_element_by_id('createSubmissionButton')
         manuscript_create_submit_continue.send_keys(Keys.RETURN)
 
@@ -110,7 +116,8 @@ class LoggingInTestCase(LiveServerTestCase):
 
         ##### ADD SUBMISSION NOTES #####
 
-        selenium.get(self.live_server_url+"/submission/1/info/")
+        
+        selenium.get(self.live_server_url+"/submission/" + str(manuscript.get_latest_submission().id) + "/info/")
         submission_info_submit_continue = selenium.find_element_by_xpath('//*[@id="generic_object_form"]/input[5]')
         submission_info_submit_continue.send_keys(Keys.RETURN)
         selenium.switch_to.alert.accept()
@@ -170,7 +177,7 @@ class LoggingInTestCase(LiveServerTestCase):
 
         ##### ADD SUBMISSION NOTES #####
 
-        selenium.get(self.live_server_url+"/submission/2/info/")
+        selenium.get(self.live_server_url + "/submission/" + str(manuscript.get_latest_submission().id) + "/info/")
         submission_info_submit_continue = selenium.find_element_by_xpath('//*[@id="generic_object_form"]/input[5]')
         submission_info_submit_continue.send_keys(Keys.RETURN)
         selenium.switch_to.alert.accept()
@@ -195,7 +202,8 @@ class LoggingInTestCase(LiveServerTestCase):
 
         ##### MANUSCRIPT LANDING REVIEW VERIFICATION #####
 
-        manuscript_review_submission = selenium.find_element_by_id('reviewSubmissionButtonMain')
+        manuscript_review_submission = selenium.find_element_by_id
+        ('reviewSubmissionButtonMain')
         manuscript_review_submission.send_keys(Keys.RETURN)
 
         ##### VERIFIER REVIEW - NO ISSUES #####
@@ -212,8 +220,227 @@ class LoggingInTestCase(LiveServerTestCase):
         dataverse_upload_manuscript_button = selenium.find_element_by_id('dataverseUploadManuscriptButtonMain')
         dataverse_upload_manuscript_button.send_keys(Keys.RETURN)
 
-        #time.sleep(500000)
 
+    #TODO: Test non-logged-in user?
+
+    # This tests most of the flow, with actions done by an admin-curator and a non-admin verifier (with access).
+    # This includes inviting the users to CORE2 via the UI
+    # Tests are also done with a verifier with no access to ensure privacy 
+    # This uses selenium-wire to get responses, which is technically bad form for selenium but is helpful for us
+    # Not tested: Edition, Dataverse, Files, Whole Tale
+    # TODO: Hardcode no edition setting
+    def test_3_user_workflow_with_access_checks(self):
+        admin_selenium = self.selenium
+
+        ##### ADMIN LOGIN #####
+
+        admin_selenium.get(self.live_server_url + "/admin")
+        self.assertIn("Log in", admin_selenium.title)
+        current_url = admin_selenium.current_url
+        username = admin_selenium.find_element_by_id('id_username')
+        password = admin_selenium.find_element_by_id('id_password')
+        username.send_keys('admin')
+        password.send_keys('password')
+        admin_login_submit = admin_selenium.find_element_by_xpath("//div[@class='submit-row']//input[@type='submit']")
+
+        admin_login_submit.send_keys(Keys.RETURN)
+
+        self.assertIn("ion | Django site admin", admin_selenium.title)
+
+        ##### ADMIN CREATE CURATOR_ADMIN #####
+
+        curator_admin = m.User.objects.create_superuser('curatoradmin', 'curatoradmin@test.test', 'password') 
+        role_c = m.Group.objects.get(name=c.GROUP_ROLE_CURATOR) 
+        role_a = m.Group.objects.get(name=c.GROUP_ROLE_AUTHOR) 
+        role_e = m.Group.objects.get(name=c.GROUP_ROLE_EDITOR) 
+        role_c.user_set.add(curator_admin) 
+        role_a.user_set.add(curator_admin) 
+        role_e.user_set.add(curator_admin) 
+
+        ##### CURATOR_ADMIN LOGIN #####
+
+        c_selenium = webdriver.Chrome()
+
+        c_selenium.get(self.live_server_url + "/admin")
+        self.assertIn("Log in", c_selenium.title)
+        current_url = c_selenium.current_url
+        username = c_selenium.find_element_by_id('id_username')
+        password = c_selenium.find_element_by_id('id_password')
+        username.send_keys('curatoradmin')
+        password.send_keys('password')
+        admin_login_submit = c_selenium.find_element_by_xpath("//div[@class='submit-row']//input[@type='submit']")
+
+        admin_login_submit.send_keys(Keys.RETURN)
+
+        self.assertIn("ion | Django site admin", c_selenium.title)
+
+        ##### CURATOR_ADMIN VERIFIER1 CREATION #####
+
+        c_selenium.get(self.live_server_url + "/site_actions/inviteverifier/")
+        c_selenium.find_element_by_id('id_first_name').send_keys('verifier1')
+        c_selenium.find_element_by_id('id_last_name').send_keys('last_name')
+        c_selenium.find_element_by_id('id_email').send_keys('verifier1@test.test')
+
+        verifier1_create_button = c_selenium.find_element_by_xpath('//*[@id="content"]/form/input[4]')
+        verifier1_create_button.send_keys(Keys.RETURN)
+
+        verifier1 = m.User.objects.get(email='verifier1@test.test')
+        verifier1.set_password('password')
+        verifier1.username = 'verifier1'
+        verifier1.is_staff = True #allows admin login without oauth
+        verifier1.save()
+
+        ##### VERIFIER1 LOGIN #####
+
+        v1_selenium = webdriver.Chrome()
+
+        v1_selenium.get(self.live_server_url + "/admin")
+        self.assertIn("Log in", v1_selenium.title)
+        current_url = v1_selenium.current_url
+        username = v1_selenium.find_element_by_id('id_username')
+        password = v1_selenium.find_element_by_id('id_password')
+        username.send_keys('verifier1')
+        password.send_keys('password')
+        admin_login_submit = v1_selenium.find_element_by_xpath("//div[@class='submit-row']//input[@type='submit']")
+
+        admin_login_submit.send_keys(Keys.RETURN)
+
+        self.assertIn("ion | Django site admin", v1_selenium.title)
+
+        ##### CURATOR_ADMIN VERIFIER2 CREATION #####
+
+        c_selenium.get(self.live_server_url + "/site_actions/inviteverifier/")
+        c_selenium.find_element_by_id('id_first_name').send_keys('verifier2')
+        c_selenium.find_element_by_id('id_last_name').send_keys('last_name')
+        c_selenium.find_element_by_id('id_email').send_keys('verifier2@test.test')
+
+        verifier2_create_button = c_selenium.find_element_by_xpath('//*[@id="content"]/form/input[4]')
+        verifier2_create_button.send_keys(Keys.RETURN)
+
+        verifier2 = m.User.objects.get(email='verifier2@test.test')
+        verifier2.set_password('password')
+        verifier2.username = 'verifier2'
+        verifier2.is_staff = True #allows admin login without oauth
+        verifier2.save()
+
+        ##### VERIFIER1 LOGIN #####
+
+        v2_selenium = webdriver.Chrome()
+
+        v2_selenium.get(self.live_server_url + "/admin")
+        self.assertIn("Log in", v2_selenium.title)
+        current_url = v2_selenium.current_url
+        username = v2_selenium.find_element_by_id('id_username')
+        password = v2_selenium.find_element_by_id('id_password')
+        username.send_keys('verifier1')
+        password.send_keys('password')
+        admin_login_submit = v2_selenium.find_element_by_xpath("//div[@class='submit-row']//input[@type='submit']")
+
+        admin_login_submit.send_keys(Keys.RETURN)
+
+        self.assertIn("ion | Django site admin", v2_selenium.title)
+
+        #############################################
+        ##### CURATOR_ADMIN MANUSCRIPT CREATION #####
+        #############################################
+
+        c_selenium.get(self.live_server_url+"/manuscript/create/")
+
+        c_selenium.find_element_by_id('id_pub_name').send_keys('pub_name')
+        c_selenium.find_element_by_id('id_pub_id').send_keys('pub_id')
+        c_selenium.find_element_by_id('id_description').send_keys('description')
+        c_selenium.find_element_by_id('id_additional_info').send_keys('additional_info')
+        c_selenium.find_element_by_id('id_contact_first_name').send_keys('curatoradmin')
+        c_selenium.find_element_by_id('id_contact_last_name').send_keys('contact_last_name')
+        c_selenium.find_element_by_id('id_contact_email').send_keys('curatoradmin@test.test')
+        Select(c_selenium.find_element_by_id('id_compute_env')).select_by_visible_text("Other") # select unneeded currently because its the only option
+        c_selenium.find_element_by_id('id_operating_system').send_keys('operating_system')
+        c_selenium.find_element_by_id('id_packages_info').send_keys('packages_info')
+        c_selenium.find_element_by_id('id_software_info').send_keys('software_info')
+        #NOTE: The selenium test doesn't seem to do JS the same, so our janky formsets don't have a row displayed unless we click
+        manuscript_create_add_author_link_button = c_selenium.find_element_by_xpath('//*[@id="author_table"]/tbody/tr[3]/td/a')
+        manuscript_create_add_author_link_button.send_keys(Keys.RETURN)
+        c_selenium.find_element_by_id('id_author_formset-0-first_name').send_keys('author_0_first_name')
+        c_selenium.find_element_by_id('id_author_formset-0-last_name').send_keys('author_0_last_name')
+        manuscript_create_add_data_source_link_button = c_selenium.find_element_by_xpath('//*[@id="data_source_table"]/tbody/tr[3]/td/a')
+        manuscript_create_add_data_source_link_button.send_keys(Keys.RETURN)
+        c_selenium.find_element_by_id('id_data_source_formset-0-text').send_keys('data_source_0_text')
+        manuscript_create_add_keyword_link_button = c_selenium.find_element_by_xpath('//*[@id="keyword_table"]/tbody/tr[3]/td/a')
+        manuscript_create_add_keyword_link_button.send_keys(Keys.RETURN)
+        c_selenium.find_element_by_id('id_keyword_formset-0-text').send_keys('keyword_0_text')
+
+        manuscript_create_submit_continue = c_selenium.find_element_by_xpath('//*[@id="generic_object_form"]/input[7]')
+        manuscript_create_submit_continue.send_keys(Keys.RETURN) 
+
+        ##### UPLOAD MANUSCRIPT FILES (skipping for now) #####
+
+        ##### CURATOR_ADMIN VERIFIER ADD #####
+
+        # NOTE: For some reason our verifiers don't show up as options for the assignverifier page
+        #       This may be because of the javascript/ajax(?)
+        #       So instead we'll assign via the backend, but this is a TODO for later
+        #       - When we do this, we may want to move where it happens in the flow
+
+        #c_selenium.get(self.live_server_url + "/manuscript/" + str(manuscript.id) + "/assignverifier/")
+
+        manuscript = m.Manuscript.objects.latest('updated_at')
+        group_manuscript_verifier = m.Group.objects.get(name=c.GROUP_MANUSCRIPT_VERIFIER_PREFIX + " " + str(manuscript.id)) 
+        group_manuscript_verifier.user_set.add(verifier1) 
+        
+        ##### TEST VERIFIER ACCESS MANUSCRIPT : NEW #####
+
+        #TODO: Test not logged in user
+        self.assertEqual(manuscript._status, m.Manuscript.Status.NEW)
+        assert_dict_manu_both = {'edit': 404, 'update': 404}
+        assert_dict_gen_both = {'manuscript_table': 404, 'manuscript/create': 404}
+        #assert_dict_sub_both = {'info': 404}
+        #time.sleep(500000)
+        self.check_access(v1_selenium, manuscript=manuscript, assert_dict=assert_dict_manu_both)
+        self.check_access(v1_selenium, assert_dict=assert_dict_gen_both)
+
+        ##### ASSIGN AUTHOR (SELF) TO MANUSCRIPT #####
+
+        c_selenium.get(self.live_server_url + "/manuscript/" + str(manuscript.id) + "/addauthor/")
+        #We don't fill out any fields for this form beacuse it is auto-populated with manuscript info
+        manuscript_add_author_submit = c_selenium.find_element_by_xpath('//*[@id="content"]/form/input[4]')
+        manuscript_add_author_submit.send_keys(Keys.RETURN)
+        c_selenium.switch_to.alert.accept()
+
+        ##### MANUSCRIPT LANDING #####
+
+        manuscript_create_submit_continue = c_selenium.find_element_by_id('createSubmissionButton')
+        manuscript_create_submit_continue.send_keys(Keys.RETURN)
+
+        # TODO create submission etc
+        #     - When are we testing access for the two verifiers?
+        #       - Ideal is when there is a sub/manuscript status change? But is that too messy?
+        #         - Maybe we should create a separate function that calls all our endpoints (pass browser (e.g. user), manuscript or submission)
+
+        time.sleep(500000)
+
+    # Not a test but a helper
+    # assert_dict example: {'edit': 200, 'update': 500}
+    def check_access(self, browser, manuscript=None, submission=None, assert_dict=None):
+        if assert_dict == None:
+            raise Exception('assert_dict must be set for check_access')            
+        if manuscript != None and submission != None:
+            raise Exception('both manuscript and submission cannot be set at the same time for check_access')
+
+        if manuscript != None:
+            url_pre = '/manuscript/' + str(manuscript.id) + '/'
+        elif submission != None:
+            url_pre = '/submission/' + str(submission.id) + '/'
+        else:
+            url_pre = '/'
+
+        for url_post, status in assert_dict.items():
+            full_url = self.live_server_url + url_pre + url_post + '/'
+            # print(full_url)
+            browser.get(full_url)
+            for request in browser.requests:
+                if request.url == full_url:
+                    self.assertEqual(request.response.status_code, status, msg=full_url)
+                    del browser.requests
 
 
 
